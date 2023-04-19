894e18770530631635ca2218c652f59144bc269b
Jay Klehr <jay@diablomedia.com>
2023-04-19 14:57:01 -0600

    php-cs-fixer fix

--- src/Zend/Session.php
+++ src/Zend/Session.php
@@ -519,11 +519,11 @@ class Zend_Session extends Zend_Session_Abstract
         $pattern = '';
         switch ($hashBitsPerChar) {
             case 4: $pattern = '^[0-9a-f]*$';
-            break;
+                break;
             case 5: $pattern = '^[0-9a-v]*$';
-            break;
+                break;
             case 6: $pattern = '^[0-9a-zA-Z-,]*$';
-            break;
+                break;
         }
         return preg_match('#' . $pattern . '#', $id);
     }
